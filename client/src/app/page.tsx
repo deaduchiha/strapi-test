eb6e457d87384983ac4429f1995aaa48bdf314e2
Alireza Nikzad <alireza.nikzdd@gmail.com>
2024-06-16 12:04:15 +0330

    feat: add section

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button";
+import Hero from "@templates/hero/Hero";
 
 export default function Home() {
   return (
-    <main>
-      <Button>hello</Button>
-    </main>
+    <div>
+      {/* Hero section */}
+      <Hero />
+    </div>
   );
 }
